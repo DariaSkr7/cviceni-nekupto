12c017d16d96a6131687b141c47a23eca1769021
Daria Skoryk <dariaskoryk@gmail.com>
2023-09-27 00:20:02 +0200

    final result 2

--- index.js
+++ index.js
@@ -19,6 +19,54 @@ const product3 = {
     'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
+const product1HTML = `<img class="card-img-top" src="${product1.image}" alt="Card image cap">
+<div class="card-body">
+  <h2 class="card-title">${product1.name}</h2>
+  <p class="card-text">${product1.description}</p>
+
+</div>
+<button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`;
+
+const product1Element = document.querySelector('#product1');
+
+product1Element.innerHTML = product1HTML;
+
+const product2HTML = `<img
+class="card-img-top"
+src="${product2.image}"
+alt="Slepičkabelka"
+/>
+<div class="card-body">
+<h2 class="card-title">${product2.name}</h2>
+<p class="card-text">${product2.description}
+</p>
+</div>
+<button type="button" class="btn btn-lg btn-primary btn-block">
+Koupit
+</button>`;
+
+const product2Element = document.querySelector('#product2');
+
+product2Element.innerHTML = product2HTML;
+
+const product3HTML = `<img
+class="card-img-top"
+src="${product3.image}"
+alt="Slepičkabelka"
+/>
+<div class="card-body">
+<h2 class="card-title">${product3.name}</h2>
+<p class="card-text">${product3.description}
+</p>
+</div>
+<button type="button" class="btn btn-lg btn-primary btn-block">
+Koupit
+</button>`;
+
+const product3Element = document.querySelector('#product3');
+
+product3Element.innerHTML = product3HTML;
+
 const firstProduct = document.querySelector('#product1');
 firstProduct.classList.add('border-primary');
 
